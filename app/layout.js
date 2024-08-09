a03361c9552e1260d6ac1547d35c640ebb315d35
amelia8872 <jkramelia8872@gmail.com>
2024-08-09 12:19:28 -0400

    add shadcn and tailwind, route page

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Customer Support",
-  description: "Welcome to Headstarter AI customer support service",
+  title: 'Customer Support',
+  description: 'Welcome to Headstarter AI customer support service',
 };
 
 export default function RootLayout({ children }) {
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      // Your custom theme configuration
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+};
